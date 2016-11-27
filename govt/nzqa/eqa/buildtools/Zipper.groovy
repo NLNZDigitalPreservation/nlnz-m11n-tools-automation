4708a19db4154f7607342d4bead0c643ad8c104f
KurtLenfesty <Kurt.Lenfesty@nzqa.govt.nz>
2016-11-27 23:53:16 +1300

    MW-28 Zipper more robust. New artifacts. Contacts split.
    Zipper now handles files and folders using temporary zip files
    and provides a much simpler API.
    upload-artifacts-to-nexus.groovy has new artifacts:
    org.codehaus.castor:castor:1.0.5, org.codehaus.castor:castor-properties:1.0.5,
    org.apache.taglibs:taglibs-standard-impl-coredefs-special:1.0.0.
    Added work-eqa-apps-contacts.groovy for splitting out eqa/apps/contacts.
    Added some nonexistant folders to eqa-apps-claims.groovy.

--- govt/nzqa/eqa/buildtools/Zipper.groovy
+++ govt/nzqa/eqa/buildtools/Zipper.groovy
@@ -1,5 +1,7 @@
 package nz.govt.nzqa.eqa.buildtools
 
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.zip.ZipOutputStream
 import java.util.zip.ZipEntry
 import java.nio.file.Files
@@ -9,22 +11,70 @@ import java.nio.file.Files
  */
 class Zipper {
 
-    def zipFolder = { folderPath, targetFilePath ->
-        File targetFile = new File(targetFilePath)
-        targetFile.parentFile.mkdirs()
-        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(targetFilePath))
-        new File(folderPath).eachFile() { file ->
-            if (!file.isFile()) {
+    def zipFolder = { folderPath, targetFile = null ->
+        return zipFiles( [ folderPath ], targetFile)
+    }
+
+    // Create a zip archive with a single file
+    def zipFile = { filePath, targetFile = null ->
+        def filesPath = [ filePath ]
+        zipFiles(filesPath, targetFile)
+    }
+
+    // Create a zip archive with a collection of files
+    // Note that it doesn't recurse...
+    // and if the targetFile is NOT specified it is deleted on JVM exit
+    def zipFiles = { filesPaths, targetFile = null ->
+        File theTargetFile = null
+        if (targetFile != null) {
+            theTargetFile = targetFile
+        } else {
+            theTargetFile = File.createTempFile("zipper-temp-zip", ".zip")
+            theTargetFile.deleteOnExit()
+        }
+
+        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(theTargetFile))
+        filesPaths.each { candidateFile ->
+            File file = getFile(candidateFile)
+            if (file == null) {
                 return
             }
-            zipOutputStream.putNextEntry(new ZipEntry(file.getName()))
-            InputStream fileInputStream = new FileInputStream(file)
-
-            // stream the file data to the ZIP
-            Files.copy(fileInputStream, zipOutputStream)
-            fileInputStream.close()
-            zipOutputStream.closeEntry()
+            if (file.isDirectory()) {
+                file.eachFile { afile ->
+                    zipEntry(afile, zipOutputStream)
+                }
+            } else {
+                zipEntry(file, zipOutputStream)
+            }
         }
         zipOutputStream.close()
+
+        return theTargetFile
+    }
+
+    def zipEntry = { file, zipOutputStream ->
+        zipOutputStream.putNextEntry(new ZipEntry(file.getName()))
+        InputStream fileInputStream = new FileInputStream(file)
+
+        // stream the file data to the ZIP
+        Files.copy(fileInputStream, zipOutputStream)
+        fileInputStream.close()
+        zipOutputStream.closeEntry()
+    }
+
+    def getFile = { fileOrPathOrAbsoluteFilePath ->
+        if (fileOrPathOrAbsoluteFilePath == null) {
+            return null
+        }
+        if (fileOrPathOrAbsoluteFilePath instanceof File) {
+            return fileOrPathOrAbsoluteFilePath
+        } else if (fileOrPathOrAbsoluteFilePath instanceof Path) {
+            return fileOrPathOrAbsoluteFilePath.toFile()
+        } else if (fileOrPathOrAbsoluteFilePath instanceof String || fileOrPathOrAbsoluteFilePath instanceof GString) {
+            return Paths.get(fileOrPathOrAbsoluteFilePath).toFile()
+        } else {
+            println("Zipper Unable to process: [${fileOrPathOrAbsoluteFilePath}] is [${fileOrPathOrAbsoluteFilePath.getClass()}]")
+        }
+        return null
     }
 }
