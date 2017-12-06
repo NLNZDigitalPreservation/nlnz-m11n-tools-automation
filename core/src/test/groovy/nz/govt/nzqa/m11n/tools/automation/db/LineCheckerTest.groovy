903a5c6d98ff13f72c1bffcb1fbde8b566c84b1e
amyl <Amy.Lin@nzqa.govt.com>
2017-12-07 10:08:21 +1300

    add unit tests and test files

--- core/src/test/groovy/nz/govt/nzqa/m11n/tools/automation/db/LineCheckerTest.groovy
+++ core/src/test/groovy/nz/govt/nzqa/m11n/tools/automation/db/LineCheckerTest.groovy
@@ -5,6 +5,7 @@ import org.junit.Test
 
 import static org.junit.Assert.assertFalse
 import static org.junit.Assert.assertTrue
+import  static org.junit.Assert.assertEquals
 
 /**
  * Tests the {@link nz.govt.nzqa.m11n.tools.automation.db.LineChecker}.
@@ -20,22 +21,45 @@ class LineCheckerTest {
 
     @Test
     void detectNoNameChangeWhenLineHasNoName() {
-        String lineTest = "GO"
+        String lineName = ''
         String currentEntityNameTest = ''
-        assertTrue("1. Detect no name change correctly when line has no name",  lineCheckerTestObj.entityNameHasChanged(lineTest, currentEntityNameTest))
+        assertFalse("Detect no name change correctly when line has no name",  lineCheckerTestObj.entityNameHasChanged(lineName, currentEntityNameTest))
     }
 
     @Test
     void detectNameChangeWhenLineHasNewName() {
-        String lineTest = "IF EXISTS (SELECT * FROM systypes WHERE name='nsn')"
+        String lineName = 'nsn'
         String currentEntityNameTest = ''
-        assertFalse("2. Detect name change correctly with new name",  lineCheckerTestObj.entityNameHasChanged(lineTest, currentEntityNameTest))
+        assertTrue("Detect name change correctly with new name",  lineCheckerTestObj.entityNameHasChanged(lineName, currentEntityNameTest))
     }
 
     @Test
-    void detectNoNameChangeWhenLineHasSameName() {
-        String lineTest = "IF EXISTS (SELECT * FROM systypes WHERE name='number_2')"
-        String currentEntityNameTest = 'number_2'
-        assertTrue("3. Detect no name change correctly",  lineCheckerTestObj.entityNameHasChanged(lineTest, currentEntityNameTest))
+    void extractLineNameCorrectlyWhenLineHasNoName() {
+        String lineTest = "GO"
+        assertEquals("Extract line name correctly", '', lineCheckerTestObj.getEntityNameFromLine(lineTest))
+    }
+
+    @Test
+    void extractLineNameCorrectlyWhenLineHasName() {
+        String lineTest = "IF EXISTS (SELECT * FROM systypes WHERE name='wwwaddr')"
+        assertEquals("Extract line name correctly", 'wwwaddr', lineCheckerTestObj.getEntityNameFromLine(lineTest))
+    }
+
+    @Test
+    void extractLineNameCorrectlyWhenLineHasName2() {
+        String lineTest = "IF OBJECT_ID('dbo.default_n') IS NOT NULL"
+        assertEquals("Extract line name correctly 2", 'dbo.default_n', lineCheckerTestObj.getEntityNameFromLine(lineTest))
+    }
+
+    @Test
+    void extractLineNameCorrectlyWhenLineHasName3() {
+        String lineTest = "IF OBJECT_ID('default_n') IS NOT NULL"
+        assertEquals("Extract line name correctly 3", 'default_n', lineCheckerTestObj.getEntityNameFromLine(lineTest))
+    }
+
+    @Test
+    void extractLineNameCorrectlyWhenLineHasName4() {
+        String lineTest = "CREATE DEFAULT dbo.Default_1 AS 0"
+        assertEquals("Extract line name correctly 4", 'dbo.Default_1', lineCheckerTestObj.getEntityNameFromLine(lineTest))
     }
 }
