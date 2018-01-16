a69ba43f07a61576ae64f6155f8aace3d09093d6
Amy Lin <Amy.Lin@nzqa.govt.nz>
2018-01-16 16:23:05 +1300

    MODIT-18
    Complete AttributeParser and unit test.

--- parser/src/main/groovy/nz/govt/nzqa/m11n/tools/automation/parser/AttributeParser.groovy
+++ parser/src/main/groovy/nz/govt/nzqa/m11n/tools/automation/parser/AttributeParser.groovy
@@ -4,56 +4,67 @@ import nz.govt.nzqa.dbmigrate.model.Attribute
 
 class AttributeParser implements Parser{
 
-    String getType(String sql){
-        String type = ''
+    String getType(String sqlStatement){
+        def result = (sqlStatement =~ /(?i)(CREATE|ALTER|ADD|DROP|DROPONLY) (\S+) (\S+) \((.*)\)/)
+        String type = (result && result[0][1].toString().equalsIgnoreCase('CREATE')? 'Column' : '')
 
         return type
     }
 
-    String getName(String sql){
-        String name = ''
+    String getName(String attributeString){
+        String[] attributeFields = attributeString.trim().replaceAll(" +", " ").split(" ")
+        String name = (attributeFields.size() > 0? attributeFields[0] : '')
 
         return name
     }
 
-    String getAction(String sql){
-        String action = ''
+    String getAction(String sqlStatement){
+        def result = (sqlStatement =~ /(?i)(CREATE|ALTER|ADD|DROP|DROPONLY) (\S+) (\S+) \((.*)\)/)
+        String action = (result? result[0][1].toString() : '')
 
         return action
     }
 
-    String getDataType(String sql){
-        String dataType = ''
+    String getDataType(String attributeString){
+        String[] attributeFields = attributeString.trim().replaceAll(" +", " ").split(" ")
+        def type = (attributeFields.size() > 0? attributeFields[1] =~ /(\S+)\((.*)\)/ : '')
+        String dataType = (type? type[0][1] : (attributeFields.size() > 0? attributeFields[1] : ''))
 
         return dataType
     }
 
-    String getLength(String sql){
-        String length = ''
-
+    String getLength(String attributeString){
+        String[] attributeFields = attributeString.trim().replaceAll(" +", " ").split(" ")
+        def type = (attributeFields.size() > 0? attributeFields[1] =~ /(\S+)\((.*)\)/ : '')
+        String[] types = (type? type[0][2].toString().split(",") : '')
+        String length =  (types.size() == 2? types[0] : type[0][2])
         return length
     }
 
-    String getFraction(String sql){
-        String fraction = ''
-
+    String getFraction(String attributeString){
+        String[] attributeFields = attributeString.trim().replaceAll(" +", " ").split(" ")
+        def type = (attributeFields.size() > 0? attributeFields[1] =~ /(\S+)\((.*)\)/ : '')
+        String[] types = (type? type[0][2].toString().split(",") : '')
+        String fraction = (types.size() == 2? types[1] : '')
         return fraction
     }
 
-    String getDefaultValue(String sql){
-        String defaultValue = ''
-
+    String getDefaultValue(String attributeString){
+        def result = (attributeString =~ /(?i)DEFAULT (\S+)/)
+        String defaultValue = (result? result[0][1].toString().replaceAll("'", "")
+                .replaceAll('"', "") : '')
         return defaultValue
     }
 
-    String getDefaultValueDataType(String sql){
-        String defaultValueDataType = ''
-
-        return defaultValueDataTypes
+    String getDefaultValueDataType(String attributeString){
+        def result = (attributeString =~ /(?i)DEFAULT (\d+)/)
+        String defaultValueDataType = (result? 'int' : 'char')
+        return defaultValueDataType
     }
 
-    boolean getIsNull(String sql){
-        boolean isNull = false
+    boolean getIsNull(String attributeString){
+        def result = (attributeString =~ /(?i)NOT NULL/)
+        boolean isNull = !(result)
 
         return isNull
     }
@@ -65,7 +76,19 @@ class AttributeParser implements Parser{
     }
 
     @Override
-    Attribute parse(String sqlStatement) {
-        return null
+    Attribute parse(String attributeString) {
+
+        Attribute attribute = new Attribute()
+
+        attribute.setName(getName(attributeString))
+
+        attribute.setDataType(getDataType(attributeString))
+        attribute.setLength(getLength(attributeString))
+        attribute.setFraction(getFraction(attributeString))
+        attribute.setDefaultValue(getDefaultValue(attributeString))
+        attribute.setDefaultValDataType(getDefaultValueDataType(attributeString))
+        attribute.setNull(getIsNull(attributeString))
+
+        return attribute
     }
 }
---
+++ parser/src/test/groovy/nz/govt/nzqa/m11n/tools/automation/parser/AttributeParserTest.groovy
@@ -0,0 +1,142 @@
+package nz.govt.nzqa.m11n.tools.automation.parser
+
+import nz.govt.nzqa.dbmigrate.model.Attribute
+import org.junit.Before
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+/**
+ * Tests the {@link nz.govt.nzqa.m11n.tools.automation.parser.EntityParser}.
+ */
+
+class AttributeParserTest {
+
+    AttributeParser attributeParser
+
+    @Before
+    void setup() {
+        attributeParser = new AttributeParser()
+    }
+
+    @Test
+    void shouldParseAttributeStringAndReturnCorrectAttribute() {
+
+        String attributeString1 = 'academic_year            char(4)    NOT NULL'
+        String attributeString2 = 'academic_year_type       varchar(4) NOT NULL'
+
+        Attribute attribute1 = new Attribute()
+        attribute1.setName('academic_year')
+        attribute1.setDataType('char')
+        attribute1.setLength('4')
+        attribute1.setNull(false)
+
+        Attribute attribute2 = new Attribute()
+        attribute2.setName('academic_year_type')
+        attribute2.setDataType('varchar')
+        attribute2.setLength('4')
+        attribute2.setNull(false)
+
+        Attribute generatedAttribute1 = attributeParser.parse(attributeString1)
+        Attribute generatedAttribute2 = attributeParser.parse(attributeString2)
+
+        assertEquals(attribute1.getName(), generatedAttribute1.getName())
+        assertEquals(attribute1.getDataType(), generatedAttribute1.getDataType())
+        assertEquals(attribute1.getLength(), generatedAttribute1.getLength())
+        assertEquals(attribute1.isNull(), generatedAttribute1.isNull())
+
+        assertEquals(attribute2.getName(), generatedAttribute2.getName())
+        assertEquals(attribute2.getDataType(), generatedAttribute2.getDataType())
+        assertEquals(attribute2.getLength(), generatedAttribute2.getLength())
+        assertEquals(attribute2.isNull(), generatedAttribute2.isNull())
+    }
+
+    @Test
+    void shouldParseAttributeCharType(){
+
+        String attributeString = 'academic_year            char(4)    NOT NULL'
+        Attribute attribute = new Attribute()
+        attribute.setName('academic_year')
+        attribute.setDataType('char')
+        attribute.setLength('4')
+        attribute.setFraction('')
+        attribute.setNull(false)
+
+        Attribute generatedAttribute = attributeParser.parse(attributeString)
+
+        assertEquals(attribute.getName(), generatedAttribute.getName())
+        assertEquals(attribute.getDataType(), generatedAttribute.getDataType())
+        assertEquals(attribute.getLength(), generatedAttribute.getLength())
+        assertEquals(attribute.getFraction(), generatedAttribute.getFraction())
+        assertEquals(attribute.isNull(), generatedAttribute.isNull())
+    }
+
+    @Test
+    void shouldParseAttributeIntType(){
+
+        String attributeString = 'academic_year            int(10,2)    NOT NULL'
+        Attribute attribute = new Attribute()
+        attribute.setName('academic_year')
+        attribute.setDataType('int')
+        attribute.setLength('10')
+        attribute.setFraction('2')
+        attribute.setNull(false)
+
+        Attribute generatedAttribute = attributeParser.parse(attributeString)
+
+        assertEquals(attribute.getName(), generatedAttribute.getName())
+        assertEquals(attribute.getDataType(), generatedAttribute.getDataType())
+        assertEquals(attribute.getLength(), generatedAttribute.getLength())
+        assertEquals(attribute.getFraction(), generatedAttribute.getFraction())
+        assertEquals(attribute.isNull(), generatedAttribute.isNull())
+    }
+
+    @Test
+    void shouldParseAttributeWithDefaultChar(){
+
+        String attributeString = 'file_type_code            char(3)   DEFAULT "DM1" NOT NULL'
+        Attribute attribute = new Attribute()
+        attribute.setName('file_type_code')
+        attribute.setDataType('char')
+        attribute.setLength('3')
+        attribute.setFraction('')
+        attribute.setDefaultValue("DM1")
+        attribute.setDefaultValDataType("char")
+        attribute.setNull(false)
+
+        Attribute generatedAttribute = attributeParser.parse(attributeString)
+
+        assertEquals(attribute.getName(), generatedAttribute.getName())
+        assertEquals(attribute.getDataType(), generatedAttribute.getDataType())
+        assertEquals(attribute.getLength(), generatedAttribute.getLength())
+        assertEquals(attribute.getFraction(), generatedAttribute.getFraction())
+        assertEquals(attribute.getDefaultValue(), generatedAttribute.getDefaultValue())
+        assertEquals(attribute.getDefaultValDataType(), generatedAttribute.getDefaultValDataType())
+        assertEquals(attribute.isNull(), generatedAttribute.isNull())
+    }
+
+    @Test
+    void shouldParseAttributeWithDefaultInt(){
+
+        String attributeString = 'file_type_code            int(10,2)   DEFAULT 30 NOT NULL'
+        Attribute attribute = new Attribute()
+        attribute.setName('file_type_code')
+        attribute.setDataType('int')
+        attribute.setLength('10')
+        attribute.setFraction('2')
+        attribute.setDefaultValue("30")
+        attribute.setDefaultValDataType("int")
+        attribute.setNull(false)
+
+        Attribute generatedAttribute = attributeParser.parse(attributeString)
+
+        assertEquals(attribute.getName(), generatedAttribute.getName())
+        assertEquals(attribute.getDataType(), generatedAttribute.getDataType())
+        assertEquals(attribute.getLength(), generatedAttribute.getLength())
+        assertEquals(attribute.getFraction(), generatedAttribute.getFraction())
+        assertEquals(attribute.getDefaultValue(), generatedAttribute.getDefaultValue())
+        assertEquals(attribute.getDefaultValDataType(), generatedAttribute.getDefaultValDataType())
+        assertEquals(attribute.isNull(), generatedAttribute.isNull())
+    }
+
+}
